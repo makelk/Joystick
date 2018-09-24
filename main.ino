cee3bc22ea80ef37dd33cc31cb962b0456d2aa75
makelk <42709957+makelk@users.noreply.github.com>
2018-09-24 02:52:33 +0100

    Add files via upload

---
+++ main.ino
@@ -0,0 +1,22 @@
+int x = A0;
+int y = A1;
+int sw = 2;
+void setup() {
+  Serial.begin(9600);
+  pinMode(x,INPUT);
+   pinMode(y,INPUT);
+    pinMode(sw,INPUT);
+}
+
+void loop() {
+  int px = analogRead(x);
+  Serial.println(px);
+  Serial.println("______");
+  Serial.println("px");
+  delay(500);
+  int py = analogRead(y);
+Serial.println(py);
+Serial.println("______");
+Serial.println("py");
+ delay(500);
+}
